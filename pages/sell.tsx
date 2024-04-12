3f5bff9ef388e461951cc6ab90741143f601af2c
hard-nett <hardnettt@proton.me>
2024-04-12 06:07:30 +0000

    comment nft stuff for now

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -1,13 +1,13 @@
-import { Layout, Wallet } from "@/components";
-import { Divider } from "@interchain-ui/react";
-import { Sell } from "@/components/sell";
+// import { Layout, Wallet } from "@/components";
+// import { Divider } from "@interchain-ui/react";
+// import { Sell } from "@/components/sell";
 
-export default function SellPage() {
-  return (
-    <Layout>
-      <Wallet />
-      <Sell />
-      <Divider mt="$16" mb="$16" />
-    </Layout>
-  )
-}
+// export default function SellPage() {
+//   return (
+//     <Layout>
+//       <Wallet />
+//       <Sell />
+//       <Divider mt="$16" mb="$16" />
+//     </Layout>
+//   )
+// }
